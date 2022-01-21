6996ef49cd7b5460593e0a8aed649ac03c2a082c
MCSQNXA <3337140142@qq.com>
2022-01-21 22:01:30 +0800

    2022-01-21-22-00

--- app/src/main/java/mcsq/nxa/timedtask/MainActivity.java
+++ app/src/main/java/mcsq/nxa/timedtask/MainActivity.java
@@ -1,9 +1,14 @@
 package mcsq.nxa.timedtask;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 /**
@@ -17,6 +22,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         super.setContentView(R.layout.main_activity);
+
+        super.findViewById(R.id.main_activity_open_yinyongxingxi).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction(android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                intent.setData(Uri.parse("package:" + view.getContext().getPackageName()));
+                view.getContext().startActivity(intent);
+            }
+        });
 
         super.findViewById(R.id.main_activity_open_wuzhangai).setOnClickListener(new View.OnClickListener() {
             @Override
